b552d2d2e1fc644b727a96665192be26dec9c9ef
Ben Stratton <benjaminjstratton@outlook.com>
2024-05-02 14:15:29 -0400

    StringBuffer example

---
+++ Java Classwork/JavaClasswork/src/main/java/StringBufferExample.java
@@ -0,0 +1,16 @@
+public class StringBufferExample {
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer("filename.txt");
+        sb.insert(0,"new_");
+
+        System.out.println(sb);
+
+        sb.replace(0,4,"old_");
+//The thing to memorize for KBA is that replace is going to first delete the items
+// from 0-4 and then insert the new string at index 0.
+        System.out.println(sb);
+
+        sb.replace(0, 4, "very_old_");
+        System.out.println(sb);
+    }
+}

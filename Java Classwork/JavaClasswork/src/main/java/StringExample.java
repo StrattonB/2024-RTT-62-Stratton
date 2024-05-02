42984f3aa0963ef2bda78c79d8d10747e697ddc3
Ben Stratton <benjaminjstratton@outlook.com>
2024-05-02 13:59:28 -0400

    More String practice

---
+++ Java Classwork/JavaClasswork/src/main/java/StringExample.java
@@ -0,0 +1,17 @@
+public class StringExample {
+    public static void main(String[] args){
+        String str = "This is a string. The more text I add, the length will increase";
+
+
+        System.out.println("The length of the string = " + str.length());
+        System.out.println("The string is empty : " + str.isEmpty());
+        System.out.println("Converted to upper case: " + str.toUpperCase());
+        System.out.println("Converted to lower case: " + str.toLowerCase());
+
+        String fileName = "document.pdf";
+        System.out.println("The file name is " + fileName + "and the length is " + fileName.length());
+        System.out.println("The period is at the index " + fileName.indexOf('.'));
+        System.out.println("The file extension is " + fileName.substring(fileName.indexOf('.')+1));
+        System.out.println("The name of the file is " + fileName.substring(2, fileName.indexOf('.')));
+    }
+}

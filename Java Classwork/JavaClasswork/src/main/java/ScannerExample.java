16d6d8dd8b96c367cfab1f0d557fcd2e12b4157e
Ben Stratton <benjaminjstratton@outlook.com>
2024-05-02 11:07:04 -0400

    boolean and instanceof added

--- Java Classwork/JavaClasswork/src/main/java/ScannerExample.java
+++ Java Classwork/JavaClasswork/src/main/java/ScannerExample.java
@@ -6,5 +6,9 @@ public class ScannerExample {
         System.out.println("Enter a number: ");
         int num1 = sc1.nextInt();
         System.out.println(num1);
+        boolean isTrue = (sc1 instanceof Scanner);
+        System.out.print("sc1 is a Scanner: "+  isTrue);
+
+
     }
 }

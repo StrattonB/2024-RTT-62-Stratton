c85d0d5103b998115201c6260af1bf7eab532fab
Ben Stratton <benjaminjstratton@outlook.com>
2024-05-03 10:06:59 -0400

    GLAB 303.3.2

---
+++ Java Classwork/JavaClasswork/src/main/java/GLAB_303_3_2/ReadingStringFromConsole.java
@@ -0,0 +1,27 @@
+package GLAB_303_3_2;
+
+import java.util.Scanner;
+
+public class ReadingStringFromConsole {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter three words separated by spaces: ");
+        String s1 = input.next();
+        String s2 = input.next();
+        String s3 = input.next();
+        System.out.println("s1 is " + s1);
+        System.out.println("s2 is " + s2);
+        System.out.println("s3 is " + s3);
+
+        System.out.println("----------Example 2----------");
+
+        Scanner inpt = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        String s = inpt.nextLine();
+        char ch = s.charAt(0);
+        System.out.println("The character entered is " + ch);
+
+
+    }
+
+}
--- Java Classwork/JavaClasswork/src/main/java/PA_303_2_4/PA_303_2_4.java
+++ Java Classwork/JavaClasswork/src/main/java/PA_303_2_4/PA_303_2_4.java
@@ -1,7 +1,7 @@
 package PA_303_2_4;
 
 public class PA_303_2_4 {
-    public class static void main(String[] args){
+    public static void main(String[] args){
 
     }
 }

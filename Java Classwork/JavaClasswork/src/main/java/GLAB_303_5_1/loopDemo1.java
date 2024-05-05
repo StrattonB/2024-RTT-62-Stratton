5a1ef2f510dc692977b2f3cb5dcbec8aa2733700
Ben Stratton <benjaminjstratton@outlook.com>
2024-05-05 10:00:36 -0400

    GLAB 303.5.1

---
+++ Java Classwork/JavaClasswork/src/main/java/GLAB_303_5_1/loopDemo1.java
@@ -0,0 +1,13 @@
+package GLAB_303_5_1;
+
+public class loopDemo1 {
+    // Program to print a text 5 times
+    public static void main(String[] args) {
+        int n = 5;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Java is fun");
+        }
+    }
+}
+

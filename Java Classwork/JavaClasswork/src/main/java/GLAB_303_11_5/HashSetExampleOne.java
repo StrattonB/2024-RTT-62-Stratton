9fe3fe7c681843be8893306d3f23dfac61bcceae
Ben Stratton <benjaminjstratton@outlook.com>
2024-05-16 14:16:58 -0400

    Completed GLAB 303.11.5

---
+++ Java Classwork/JavaClasswork/src/main/java/GLAB_303_11_5/HashSetExampleOne.java
@@ -0,0 +1,20 @@
+package GLAB_303_11_5;
+
+import java.util.HashSet;
+
+public class HashSetExampleOne {
+    public static void main(String[] args){
+        HashSet<Integer> evenNumber = new HashSet<>();
+        //using add() method
+        evenNumber.add(2);
+        evenNumber.add(4);
+        evenNumber.add(6);
+        System.out.println("HashSet: " + evenNumber);
+
+        HashSet<Integer> numbers = new HashSet<>();
+        //using addAll() method
+        numbers.addAll(evenNumber);
+        numbers.add(5);
+        System.out.println("New HashSet: "+ numbers);
+    }
+}
